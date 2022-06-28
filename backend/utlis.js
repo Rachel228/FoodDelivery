598ca96b6f36c6f4cfd919d0c8914003370bd9a0
Yana Bondarchuk <75263925+Rachel228@users.noreply.github.com>
2022-06-28 20:08:18 +0300

    Update utlis.js

--- backend/utlis.js
+++ backend/utlis.js
@@ -11,7 +11,7 @@ export const genrateToken =(user)=>{
 }
 export const isAuth=(req,res,next)=>{
     const authorization=req.headers.authorization
-    // console.log(req.headers.authorization)
+
     if(authorization){
       const token = authorization.slice(7,authorization.length) //bearer token value
       jwt.verify(token,'BhaiKaBirthday',(err,decode)=>{
